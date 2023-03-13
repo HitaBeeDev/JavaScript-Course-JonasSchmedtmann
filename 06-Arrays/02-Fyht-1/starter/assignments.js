9d6cce2f3c2eecbb4bd4e737bdf9b1faf0f44d8b
Anahita A <126061044+HitaBeeDev@users.noreply.github.com>
2023-03-13 16:17:37 +0300

    first commit

---
+++ 06-Arrays/02-Fyht-1/starter/assignments.js
@@ -0,0 +1,96 @@
+// let continet = 'Europe';
+// console.log(continet);
+
+// const country = 'Turkiye';
+// console.log(country);
+
+// let populationTr = 15 ;
+
+// console.log(populationTr);
+
+// let finlandPop = 6;
+// console.log(populationTr > finlandPop);
+
+// let langauge = "Turkish";
+
+// let avrageCnt = 33;
+// console.log(populationTr > avrageCnt );
+
+
+// if (populationTr >= 33) {
+//     console.log('Turkiye population is above average.')
+// } else {
+//     console.log(`Turkiye population is ${33 - populationTr} below average. ` )
+// }
+
+
+
+// let description = country + '' + ' is in ' + continet + ' , ' + 'its ' + populationTr + ' million people speak ' + langauge + '.';
+// console.log(description);
+
+// let description = `${country} is in ${continet} , it's ${populationTr} milion people speak ${langauge} .`
+
+// console.log(description);
+
+
+/* let isIsLand = true;
+let langauge = "Turkish";
+
+console.log(typeof isIsLand);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof langauge); */
+
+
+
+// const numNeighbours = Number( prompt('How many neighbour countries does your country have?'),
+// );
+
+// if (numNeighbours === 1) {
+//     console.log('Only 1 border!');
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border');
+// } else { console.log('No borders');
+// }
+
+
+
+// let continet = 'Europe';
+// const country = 'Turkiye';
+// let populationTr = 80 ;
+// let langauge = "Turkish";
+// let isNotIsland = true;
+
+// if (langauge === 'English' && populationTr < 50 && isNotIsland) {
+//     console.log('You should live in Turkiye!');
+// } else {
+//     console.log('Turkiye is not a suitable country for you!');
+// }
+
+
+
+
+
+
+// const language = 'spanish';
+
+// switch(language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('MOST number of native speakers!');
+//     break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//     break;
+//     case 'english':
+//         console.log('3rd place');
+//     break;
+//     case 'hindi':
+//         console.log('Number 4');
+//     break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//     break;
+//     default:
+//         console.log("for all other simply log 'Great language too :D");
+// }
---
+++ 06-Arrays/02-Fyht-1/starter/script.js
@@ -0,0 +1,7 @@
+//let js = 'amazing';
+
+//console.log(40+50+30+10);
+//console.log('Anahita');
+//console.log('Amiri');
+//console.log('Anahita is happy today');
+
